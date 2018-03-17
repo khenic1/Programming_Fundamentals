470fc8858ad3ad4e64592a0bde41343f3cee69b9
Kelley Henican <kelleyhenican2@gmail.com>
2018-03-17 13:55:45 -0500

    add loan calculator

---
+++ lesson_2/mortgage_calculator.rb
@@ -0,0 +1,65 @@
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+loop do
+  prompt("Welcome to Mortgage Calculator!")
+
+  prompt("What is the loan amount?")
+
+  loan_amount = ''
+  loop do
+    loan_amount = Kernel.gets().chomp()
+
+    if loan_amount.empty?() || loan_amount.to_f() < 0
+      prompt("Must enter positive number.")
+    else
+      break
+    end
+  end
+
+  prompt("What is the interest rate?")
+  prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
+
+  interest_rate = ''
+  loop do
+    interest_rate = Kernel.gets().chomp()
+
+    if interest_rate.empty?() || interest_rate.to_f() < 0
+      prompt("Must enter positive number.")
+    else
+      break
+    end
+  end
+
+  prompt("What is the loan duration (in years)?")
+
+  years = ''
+  loop do
+    years = Kernel.gets().chomp()
+
+    if years.empty?() || years.to_i() < 0
+      prompt("Enter a valid number")
+    else
+      break
+    end
+  end
+
+  annual_interest_rate = interest_rate.to_f() / 100
+  monthly_interest_rate = annual_interest_rate / 12
+  months = years.to_i() * 12
+
+  monthly_payment = loan_amount.to_f() *
+                    (monthly_interest_rate /
+                    (1 - (1 + monthly_interest_rate)**(-months.to_i())))
+
+  prompt("Your monthly payment is: $#{format('%02.2f', monthly_payment)}")
+
+  prompt("Another calculation?")
+  answer = Kernel.gets().chomp()
+
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the Mortgage Calculator!")
+prompt("Good bye!")
